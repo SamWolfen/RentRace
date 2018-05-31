c4f7b5e26c5d3d4407b1407eff9cac5a9456f61b
Sam Hernandez <SamuelJohnHernandez@gmail.com>
2018-05-31 15:01:55 +1000

    Working on deposit mechanic for bank

--- Assets/Plugins/SimpleInput/Example/ExamplePlayerController.cs
+++ Assets/Plugins/SimpleInput/Example/ExamplePlayerController.cs
@@ -1,31 +1,31 @@
 ﻿using UnityEngine;
 
-public class ExamplePlayerController : MonoBehaviour 
+public class ExamplePlayerController : MonoBehaviour
 {
-	private Rigidbody2D m_rigidbody;
+    private Rigidbody2D m_rigidbody;
 
-	public string horizontalAxis = "Horizontal";
-	public string verticalAxis = "Vertical";
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
     public float speed;
-	
 
-	private float inputHorizontal;
-	private float inputVertical;
+
+    private float inputHorizontal;
+    private float inputVertical;
     private int direction = 0;
     private Vector2 movement;
 
-	void Awake()
-	{
+    void Awake()
+    {
         m_rigidbody = GetComponent<Rigidbody2D>();
 
-	}
+    }
 
-	void Update()
-	{
-        
+    void Update()
+    {
+
         //check input
-		inputHorizontal = SimpleInput.GetAxis( horizontalAxis );
-		inputVertical = SimpleInput.GetAxis( verticalAxis );
+        inputHorizontal = SimpleInput.GetAxis(horizontalAxis);
+        inputVertical = SimpleInput.GetAxis(verticalAxis);
 
         if (inputHorizontal > 0)
         { direction = 1; };
@@ -51,51 +51,37 @@ public class ExamplePlayerController : MonoBehaviour
                 break;
 
             case 1:
-                movement = new Vector2(0.1f, 0f);
-                if (NotObstructed(movement))
-                {
-                    transform.Translate(speed * Time.deltaTime, 0f, 0f);
-                }
+
+                transform.Translate(speed * Time.deltaTime, 0f, 0f);
+
                 break;
             case 2:
-                movement = new Vector2(-0.1f, 0f);
-                if (NotObstructed(movement))
-                {
-                    transform.Translate(-speed * Time.deltaTime, 0f, 0f);
-                }
+
+                transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+
                 break;
             case 3:
-                movement = new Vector2(0f, speed * Time.deltaTime);
-                if (NotObstructed(movement))
-                {
-                    transform.Translate(0f, speed * Time.deltaTime, 0f);
-                }
+
+                transform.Translate(0f, speed * Time.deltaTime, 0f);
+
                 break;
 
             case 4:
-                movement = new Vector2(0f, -speed * Time.deltaTime);
-                if (NotObstructed(movement))
-                {
-                    transform.Translate(0f, -speed * Time.deltaTime, 0f);
-                }
+
+                transform.Translate(0f, -speed * Time.deltaTime, 0f);
+
                 break;
 
         }
 
-        
+
 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       // transform.Translate(0f, 0f, 0f);
+        // transform.Translate(0f, 0f, 0f);
     }
 
-    private bool NotObstructed(Vector2 dir)
-    {
-        // Cast Line from 'next to Pac-Man' to 'Pac-Man'
-        Vector2 pos = transform.position;
-        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
-        return (hit.collider == GetComponent<Collider2D>());
-    }
+
 }
--- Assets/Scripts/ScoreHolder.cs
+++ Assets/Scripts/ScoreHolder.cs
@@ -6,10 +6,17 @@ public class ScoreHolder : MonoBehaviour {
     public int Score = 0;
     public int Cash = 0;
     public int Bank = 0;
+    private float generalTimer = 0;
+    private float generalCounter = 0;
+    private float interestTimer = 0;
+    private float progressTimer = 0;
+    private string enteredBuilding = null;
+    private IEnumerator coroutine;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+
+    void Start () {
         Score = 0;
         Cash = 0;
         Bank = 0;
@@ -26,19 +33,84 @@ public class ScoreHolder : MonoBehaviour {
     public void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.name);
-        if (collision.tag == "Coin")
+        progressTimer = 0;
+        
+        switch (collision.tag)
         {
-            CoinGet(this);
-            //Probably not efficient to destroy, but can replace with pool later
-            DestroyObject(GameObject.Find(collision.name));
+            case "Coin":
+                CoinGet(this);
+                //Probably not efficient to destroy, but can replace with pool later
+                DestroyObject(GameObject.Find(collision.name));
+                break;
+
+            case "Bank":
+                //deposit money at a rate
+                coroutine = BuildingAction("Bank");
+                
+                break;
+
+        }
+
+        if (collision.tag != "Coin")
+        {
+            StartCoroutine(coroutine);
         }
 
     }
 
+    
     public void CoinGet(ScoreHolder scoreHolder)
     {
         //increases score
         scoreHolder.Cash = scoreHolder.Cash + 10;
         scoreHolder.Score = scoreHolder.Cash + scoreHolder.Bank;
+    }
+
+    private void Invest(string BuildingType)
+    {
+        switch (BuildingType)
+        {
+            case "Bank":
+                Bank += Cash;
+                Cash = 0;
+                Score = Cash + Bank;
+            break;
+        }
+    }
+
+    private IEnumerator BuildingAction(string building)
+    {
+        bool actionComplete = false;
+        float Progress = 0;
+        float requiredProgress = 0;
+
+        switch (building)
+        {
+            case "Bank":
+                requiredProgress = Cash;
+                break;
+              
+        }
+
+        while (actionComplete == false)
+        {
+
+
+            //action
+            Progress = Progress + Time.deltaTime * 10;
+            Debug.Log(Progress);
+            //test if action is complete
+            if (Progress >= requiredProgress)
+            {
+                Invest(building);
+                actionComplete = true;
+            };
+
+
+
+
+        }
+
+        return null; 
     }
 }
