76b868199a8ed879b3f609ca063a42b7eba42499
Sam Hernandez <SamuelJohnHernandez@gmail.com>
2018-07-12 16:12:19 +1000

    created a button to take the player from the title screen to the game

---
+++ Assets/MenuButtonBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public enum ButtonOption
+{
+    Play, Credits, Quit, Highscores 
+}
+
+public class MenuButtonBehavior : MonoBehaviour {
+
+    public ButtonOption buttonOption;
+    public Button button;
+
+	// Use this for initialization
+	void Start () {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+	}
+	
+	void TaskOnClick()
+    {
+        
+
+        switch (buttonOption)
+        {
+            case ButtonOption.Play:
+                SceneManager.LoadScene(sceneName: "MainScene");
+                break;
+        }
+    }
+}
