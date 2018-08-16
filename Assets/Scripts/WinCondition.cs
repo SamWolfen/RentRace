44787f93f7b889fa4061f275893caa19e18cf883
Sam Hernandez <samueljohnhernandez@gmail.com.au>
2018-08-16 13:11:07 +1000

    Win notification implemented

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject WinText;
+    float score = 0;
+    public float scoreGoal;
+    public bool win;
+    // Use this for initialization
+    void Start()
+    {
+        win = false;
+        WinText.SetActive(false);
+        StartCoroutine(Checker());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator Checker()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(1);
+
+            score = Player.GetComponent<ScoreManagerAndInteraction>().Score;
+
+
+            if (score >= scoreGoal)
+            {
+                win = true;
+            }
+
+            if (win == true)
+            {
+                //trigger win screen and end level
+                WinText.SetActive(true);
+
+            }
+            
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/ZeroSpawner.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ZeroSpawner : MonoBehaviour {
+
+    public GameObject ZeroOne;
+    public GameObject ZeroTwo;
+    public GameObject ZeroThree;
+    public int spawnCounter;
+    float x;
+    float y;
+    Vector2 spawnPoint;
+    public bool allZerosCollected;
+    public int zerosCollected;
+
+
+	// Use this for initialization
+	void Start () {
+        spawnCounter = 0;
+        ZeroOne.SetActive(false);
+        ZeroTwo.SetActive(false);
+        ZeroThree.SetActive(false);
+        zerosCollected = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (zerosCollected == 3)
+        {
+            allZerosCollected = true;
+            //call the police
+
+        }
+	}
+
+    public void SpawnZero()
+    {
+       x = Random.Range(-4.7f, 4.7f);
+       y = Random.Range(-4.7f, 4.7f);
+       spawnPoint = new Vector2(x, y);
+
+        while (IsPointOccupied(spawnPoint))
+        {
+            x = Random.Range(-4.7f, 4.7f);
+            y = Random.Range(-4.7f, 4.7f);
+            spawnPoint = new Vector2(x, y);
+        }
+
+        if (!IsPointOccupied(spawnPoint))
+        {
+            switch (spawnCounter)
+            {
+                case 1:
+                    ZeroOne.SetActive(true);
+                    ZeroOne.transform.position = spawnPoint;
+                    break;
+                case 2:
+                    ZeroTwo.SetActive(true);
+                    ZeroTwo.transform.position = spawnPoint;
+                    break;
+                case 3:
+                    ZeroThree.SetActive(true);
+                    ZeroThree.transform.position = spawnPoint;
+                    break;
+
+            }
+
+        }
+    }
+
+    bool IsPointOccupied(Vector3 point)
+    {
+        var hitColliders = Physics2D.CircleCast(point, 0.1f, new Vector2(0, 0));
+        if (hitColliders.collider)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
